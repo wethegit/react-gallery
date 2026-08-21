f03ad80ca518eaca663965bd07e6e8cd0247c9bf
Clinton <80900245+pravton@users.noreply.github.com>
2026-08-20 20:07:18 -0400

    chore: converted useGallery hook to ts

--- src/lib/hooks/use-gallery.tsx
+++ src/lib/hooks/use-gallery.tsx
@@ -1,13 +1,14 @@
 import { useContext } from "react"
 
 import { GalleryContext } from "../components/gallery-context"
+import { GalleryContextValue } from "../types/types"
 
-export const useGallery = () => {
+export const useGallery = <T,>(): GalleryContextValue<T> => {
   const context = useContext(GalleryContext)
 
   if (!context) {
     throw new Error("useGallery must be called from within a <Gallery>.")
   }
 
-  return context
+  return context as GalleryContextValue<T>
 }
