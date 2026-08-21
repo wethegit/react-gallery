8555baa7669079208818c95e13c56a613402502b
Clinton <80900245+pravton@users.noreply.github.com>
2026-08-21 13:47:34 -0400

    chore: coverts gallery nav to tsx

--- src/lib/components/gallery-nav.tsx
+++ src/lib/components/gallery-nav.tsx
@@ -1,17 +1,34 @@
 "use client"
 
+import { ComponentPropsWithRef, ReactNode } from "react"
 import { useGallery } from "../hooks/use-gallery"
-import classnames from "../utils/classnames"
+import { classnames } from "../utils/classnames"
+
+import type { RenderNavItemArgs } from "../types/types"
 
 import styles from "./gallery.module.css"
 
+export interface GalleryNavProps extends ComponentPropsWithRef<"button"> {
+  direction: 0 | 1
+  renderNavItem?: ({ activeIndex, disabled }: RenderNavItemArgs) => ReactNode
+}
+
+/**
+ * GalleryMain Component
+ * ---
+ * @example
+ * <Gallery items={GALLERY_ITEMS}>
+ *  <GalleryNav direction={0}>⬅️</GalleryNav>
+ *  <GalleryNav direction={1}>➡️</GalleryNav>
+ * </Gallery>
+ */
 export const GalleryNav = ({
   direction,
   renderNavItem,
   className,
   children,
   ...props
-}) => {
+}: GalleryNavProps) => {
   const { next, previous, loop, activeIndex, galleryItems } = useGallery()
 
   const handleClick = () => {
@@ -27,8 +44,8 @@ export const GalleryNav = ({
     (!direction && activeIndex === 0)
 
   const disabledProps = {
-    disabled: shouldDisable ? true : null,
-    "aria-disabled": shouldDisable ? "true" : null,
+    disabled: shouldDisable ? true : undefined,
+    "aria-disabled": shouldDisable ? ("true" as const) : undefined,
   }
 
   if (!children && !renderNavItem) return null
@@ -46,7 +63,7 @@ export const GalleryNav = ({
     >
       {children
         ? children
-        : renderNavItem({ activeIndex, disabled: !loop && shouldDisable })}
+        : renderNavItem?.({ activeIndex, disabled: !loop && shouldDisable })}
     </button>
   )
 }
